e25e53d11344ddf091f9e6cfa0427b988e2fbb90
YuSidorin <xepo@mail.ru>
2018-02-04 00:34:58 +0300

    Square&Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main (String[] args) {
-	 System.out.println("Hello World");
+
+	  Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной "  + s.l + " = " + s.area());
+
+	  Rectangle r = new Rectangle(4, 5);
+	 System.out.println("Площадь квадрата со сторонами "  + r.a + " и " + r.b + " = " + r.area());
+
 	}
 }
