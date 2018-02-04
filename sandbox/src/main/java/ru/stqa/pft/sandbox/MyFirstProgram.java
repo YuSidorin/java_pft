55aa7b49b31ffef8eca72254a0523ed62e50a4d9
YuSidorin <xepo@mail.ru>
2018-02-04 16:20:28 +0300

    Задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,21 +3,16 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	public static void main (String[] args) {
 
-	  Point p1 = new Point();
-    p1.x1 = 2;
-    p1.y1 = 1;
-    Point p2 = new Point();
-    p2.x2 = 3;
-    p2.y2 = 5;
-    System.out.println("Расстоние между точками  p1(" + p1.x1 + "," + p1.y1 + ") и p2(" + p2.x2 + "," + p2.y2 + ") = " + distance(p1, p2));
+	  Point p1 = new Point(1, 1);
+    Point p2 = new Point(4, 1);
+    System.out.println("Расстоние между точками  p1(" + p1.x + "," + p1.y + ") и p2(" + p2.x + "," + p2.y + ") = " + p1.distance(p1, p2));
 
 	  Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной "  + s.l + " = " + s.area());
 
 	  Rectangle r = new Rectangle(4, 5);
-	 System.out.println("Площадь квадрата со сторонами "  + r.a + " и " + r.b + " = " + r.area());
+	  System.out.println("Площадь квадрата со сторонами "  + r.a + " и " + r.b + " = " + r.area());
 
 	}
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x2 - p1.x1)*(p2.x2 - p1.x1)+(p2.y2 - p1.y1)*(p2.y2 - p1.y1));
-}}
+
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,9 +1,14 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public int x1;
-  public int x2;
-  public int y1;
-  public int y2;
+  public int x;
+  public int y;
 
+  public Point (int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+  public double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
 }
