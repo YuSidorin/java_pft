5a04096510321a0d3f042f44a3a6d119249776af
YuSidorin <xepo@mail.ru>
2018-04-13 02:26:43 +0300

    fix

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adderssbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adderssbook/tests/GroupModificationTests.java
@@ -22,13 +22,15 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
     Groups before = app.db().groups();
     GroupData modifiedGroup = before.iterator().next();
-    GroupData group = new GroupData().
-            withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
+    GroupData group = new GroupData()
+            .withId(modifiedGroup.getId()) .withName("test1") .withHeader ("test2"). withFooter("test3");
     app.goTo().GroupPage();
     app.group().modify(group);
     assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.db().groups();
+
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
+
     verifyGroupListInUI();
     }
 
